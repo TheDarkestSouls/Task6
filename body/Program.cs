9d56b4288c6aad46b1605d03b464d01e6dc0aa01
NikitaP <unntiss@bk.ru>
2022-08-07 18:37:48 +0300

    done

--- body/Program.cs
+++ body/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0) Console.Write("Yes");
+else Console.Write("No");
